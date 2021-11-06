e8dbf4021629d4755a766dd18523d0fa219c3f8d
Ovchinnikov Maksim <maksov4@gmail.com>
2021-11-06 23:31:38 +0500

    Solved task_7_1

--- 07_files/task_7_1.py
+++ 07_files/task_7_1.py
@@ -14,3 +14,25 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+"""
+info = ospf_route.split(',')
+ip = info[0].lstrip().split(' ')
+ip.extend(info[1:])
+ip.remove('via')
+ip[1] = ip[1].strip('[').strip(']')
+"""
+ospf_route_info='''
+Prefix              {}
+AD/Metric           {}
+Next-Hop            {}
+Last update         {}
+Outbound Interface  {}   
+'''
+with open('ospf.txt', 'r') as f:
+    for line in f:
+        info, lupdate, ointfr = line.split(',')
+        info = info.split(' ')
+        info.reverse()
+        nhop, _, metric, prefix, *other = info
+        metric = metric.strip('[').strip(']')
+        print(ospf_route_info.format(prefix, metric, nhop, lupdate.lstrip(), ointfr.lstrip()))
